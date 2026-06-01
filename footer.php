f8e8b0a28ec60502328b14fb0af5787d153ed492
sprokkel78 <sprokkel78.bart@gmail.com>
2026-06-01 02:37:02 +0200

    added url

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div style="width: 98%; font-size: 22; margin: 0 auto; padding: 10px; border: 2px solid #000; color: #f60; background-color: #000;">
-            Keep Growing.
+            Keep Growing. - https://www.github.com/sprokkel78/
 </div>
 <br>
 </body>
